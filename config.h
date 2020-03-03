9b3f93d4ae193e41c6988f78799c689de0716f4b
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2020-03-03 13:55:47 -0300

    Initial comit for my build of dmenu

--- config.h
+++ config.h
@@ -6,17 +6,16 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrains Mono:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeMid] = { "#eeeeee", "#770000" },
+	[SchemeNorm] = { "#bbbbbb", "#2E3440" },
+	[SchemeSel] = { "#eeeeee", "#3B4252" },
+	[SchemeSelHighlight] = { "#D8DEE9", "#005577" },
+	[SchemeNormHighlight] = { "#D8DEE9", "#3B4252" },
+	[SchemeOut] = { "#000000", "#434C5E" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
@@ -29,4 +28,4 @@ static unsigned int lineheight = 0;         /* -h option; minimum height of a me
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 4;
